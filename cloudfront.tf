7e259887d583f2af08b509ab852611d8ee31997c
Jakub Jindra <jakub.jindra@cookielab.io>
2026-01-13 08:07:21 +0100

    custom_error_response

--- cloudfront.tf
+++ cloudfront.tf
@@ -17,7 +17,7 @@ module "cdn" {
       origin_path               = var.origin_path
       origin_access_control_key = "s3"
     }
-    # TODO: tady budou dalsi dynamicky originy 
+    # TODO: tady budou dalsi dynamicky originy
     # iterovany for/for_each nad var.proxy.paths
     # a take var.oidc
   }
@@ -50,11 +50,22 @@ module "cdn" {
 
   }
 
-  # TODO: dynamic custom_error_response
-  # TODO: default_cache_behavior
+  custom_error_response = length(var.oidc) > 0 ? [] : [
+    {
+      error_code         = 404
+      response_code      = var.override_status_code_404
+      response_page_path = "/index.html"
+    },
+    {
+      error_code         = 403
+      response_code      = var.override_status_code_403
+      response_page_path = "/index.html"
+    }
+  ]
+
   # TODO: dynamic ordered_cache_behavior 2x ruzna pro var.oidc a var.proxy_paths
   # TODO: dynamic logging_config
-  #
+
   origin_access_control = {
     s3 = {
       name             = "Access from CF to S3 - ${local.main_domain}"
