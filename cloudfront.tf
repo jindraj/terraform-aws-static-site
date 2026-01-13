9f5713230cf03c6b12c50e3d28c1e1d5c258f060
Jakub Jindra <jakub.jindra@cookielab.io>
2026-01-13 09:08:33 +0100

    ordered_cache_behaviour (oidc only, proxy_paths missing)

--- cloudfront.tf
+++ cloudfront.tf
@@ -13,17 +13,36 @@ module "cdn" {
   custom_error_response = length(var.oidc) > 0 ? [] : [
     {
       error_caching_min_ttl = 3000
-      error_code            = 404
-      response_code         = var.override_status_code_404
+      error_code            = 403
+      response_code         = var.override_status_code_403
       response_page_path    = "/index.html"
     },
     {
       error_caching_min_ttl = 3000
-      error_code            = 403
-      response_code         = var.override_status_code_403
+      error_code            = 404
+      response_code         = var.override_status_code_404
       response_page_path    = "/index.html"
     }
   ]
+
+  # TODO: dynamic ordered_cache_behavior 2x ruzna pro var.oidc a var.proxy_paths
+  # ordered_cache_behavior list(object({ allowed_methods = optional(list(string), ["GET", "HEAD", "OPTIONS"]) cached_methods = optional(list(string), ["GET", "HEAD"]) cache_policy_id = optional(string) cache_policy_name = optional(string) compress = optional(bool, true) default_ttl = optional(number) field_level_encryption_id = optional(string) forwarded_values = optional(object({ cookies = object({ forward = optional(string, "none") whitelisted_names = optional(list(string)) }) headers = optional(list(string)) query_string = optional(bool, false) query_string_cache_keys = optional(list(string)) }), { cookies = { forward = "none" } query_string = false } ) function_association = optional(map(object({ event_type = optional(string) function_arn = optional(string) function_key = optional(string) }))) grpc_config = optional(object({ enabled = optional(bool) })) lambda_function_association = optional(map(object({ event_type = optional(string) include_body = optional(bool) lambda_arn = string }))) max_ttl = optional(number) min_ttl = optional(number) origin_request_policy_id = optional(string) origin_request_policy_name = optional(string) path_pattern = string realtime_log_config_arn = optional(string) response_headers_policy_id = optional(string) response_headers_policy_key = optional(string) response_headers_policy_name = optional(string) smooth_streaming = optional(bool) target_origin_id = string trusted_key_groups = optional(list(string)) trusted_signers = optional(list(string)) viewer_protocol_policy = string }))
+
+  ordered_cache_behavior = concat(
+    [],
+    length(var.oidc) == 0 ? [] : [
+      {
+        path_pattern             = "/callback*"
+        target_origin_id         = "api-gateway-origin"
+        allowed_methods          = ["GET", "HEAD", "OPTIONS"]
+        cached_methods           = ["GET", "HEAD"]
+        viewer_protocol_policy   = "redirect-to-https"
+        compress                 = true
+        cache_policy_id          = aws_cloudfront_cache_policy.oidc[0].id
+        origin_request_policy_id = aws_cloudfront_origin_request_policy.oidc[0].id
+      }
+    ]
+  )
 
   origin = {
     s3_bucket = {
@@ -31,6 +50,18 @@ module "cdn" {
       origin_id                 = var.s3_bucket_name
       origin_path               = var.origin_path
       origin_access_control_key = "s3"
+    }
+    oidc_callback = length(var.oidc) == 0 ? {} : {
+      domain_name = split("/", module.oidc.oidc_callback_url_base)[2]
+      origin_id   = "api-gateway-origin"
+      custom_origin_config = [
+        {
+          http_port              = 80
+          https_port             = 443
+          origin_protocol_policy = "https-only"
+          origin_ssl_protocols   = ["TLSv1.2"]
+        }
+      ]
     }
     # TODO: tady budou dalsi dynamicky originy
     # iterovany for/for_each nad var.proxy.paths
@@ -72,10 +103,7 @@ module "cdn" {
         include_body = false
       }
     }
-
   }
-
-  # TODO: dynamic ordered_cache_behavior 2x ruzna pro var.oidc a var.proxy_paths
 
   logging_config = var.logs_bucket_domain_name == null ? null : {
     bucket          = var.logs_bucket_domain_name
