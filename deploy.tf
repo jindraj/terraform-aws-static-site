9d13eb29b7a063869be6676bfcaea0c6f462c406
Jakub Jindra <jakub.jindra@cookielab.io>
2026-01-13 13:00:27 +0100

    gitlab: clenaup

--- deploy.tf
+++ deploy.tf
@@ -33,30 +33,6 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
-resource "aws_iam_role" "deploy" {
-  count              = var.enable_deploy_role ? 1 : 0
-  assume_role_policy = data.aws_iam_policy_document.assume_role[0].json
-  name               = "zvirt-${local.main_domain_sanitized}-deploy"
-  tags               = var.tags
-}
-
-resource "aws_iam_role_policy" "deploy" {
-  count  = var.enable_deploy_role ? 1 : 0
-  name   = "S3Deploy-CFInvalidate"
-  role   = aws_iam_role.deploy[0].id
-  policy = data.aws_iam_policy_document.deploy[0].json
-}
-
-resource "aws_iam_user" "deploy" {
-  count = var.enable_deploy_user ? 1 : 0
-  name  = "zvirt-${local.main_domain_sanitized}-deploy"
-}
-
-resource "aws_iam_access_key" "deploy" {
-  count = var.enable_deploy_user ? 1 : 0
-  user  = aws_iam_user.deploy[0].name
-}
-
 data "aws_iam_policy_document" "deploy" {
   count = var.enable_deploy_user || var.enable_deploy_role ? 1 : 0
   statement {
@@ -82,6 +58,31 @@ data "aws_iam_policy_document" "deploy" {
   }
 }
 
+resource "aws_iam_role" "deploy" {
+  count              = var.enable_deploy_role ? 1 : 0
+  assume_role_policy = data.aws_iam_policy_document.assume_role[0].json
+  name               = "zvirt-${local.main_domain_sanitized}-deploy"
+  tags               = var.tags
+}
+
+resource "aws_iam_role_policy" "deploy" {
+  count  = var.enable_deploy_role ? 1 : 0
+  name   = "S3Deploy-CFInvalidate"
+  role   = aws_iam_role.deploy[0].id
+  policy = data.aws_iam_policy_document.deploy[0].json
+}
+
+resource "aws_iam_user" "deploy" {
+  count = var.enable_deploy_user ? 1 : 0
+  name  = "zvirt-${local.main_domain_sanitized}-deploy"
+  tags  = var.tags
+}
+
+resource "aws_iam_access_key" "deploy" {
+  count = var.enable_deploy_user ? 1 : 0
+  user  = aws_iam_user.deploy[0].name
+}
+
 resource "aws_iam_user_policy" "deploy" {
   count = var.enable_deploy_user ? 1 : 0
 
@@ -95,49 +96,43 @@ module "gitlab" {
   source = "./modules/gitlab"
 
   gitlab_project_ids = local.gitlab_project_ids
-  gitlab_environment = var.gitlab_environment
-
-  #enable_deploy_role             = var.enable_deploy_role
-  #enable_deploy_user             = var.enable_deploy_user
-  #extra_gitlab_cicd_variables    = var.extra_gitlab_cicd_variables
-  #aws_s3_bucket_name             = module.s3_bucket.s3_bucket_id
-  #aws_cloudfront_distribution_id = module.cdn.cloudfront_distribution_id
-  #aws_role_arn                   = var.enable_deploy_role ? aws_iam_role.deploy[0].arn : null
-  #aws_access_key_id              = var.enable_deploy_user ? aws_iam_access_key.deploy[0].id : null
-  #aws_secret_access_key          = var.enable_deploy_user ? aws_iam_access_key.deploy[0].secret : null
-  #aws_default_region             = data.aws_region.current.region
-  #aws_env_vars_suffix            = var.aws_env_vars_suffix
 
   extra_gitlab_cicd_variables = concat(
     [
       {
         key   = "AWS_S3_BUCKET${var.aws_env_vars_suffix}"
         value = module.s3_bucket.s3_bucket_id
+        gitlab_environment = var.gitlab_environment
       },
       {
         key   = "AWS_DEFAULT_REGION${var.aws_env_vars_suffix}"
         value = data.aws_region.current.region
+        gitlab_environment = var.gitlab_environment
       },
       {
         key   = "AWS_CF_DISTRIBUTION_ID${var.aws_env_vars_suffix}"
         value = module.cdn.cloudfront_distribution_id
+        gitlab_environment = var.gitlab_environment
       },
     ],
     var.enable_deploy_role ? [
       { # CONDITIONAL
         key   = "AWS_ROLE_ARN${var.aws_env_vars_suffix}"
         value = aws_iam_role.deploy[0].arn
+        gitlab_environment = var.gitlab_environment
       }
     ] : [],
     var.enable_deploy_user ? [
       { # CONDITIONAL
         key   = "AWS_ACCESS_KEY_ID${var.aws_env_vars_suffix}"
         value = aws_iam_access_key.deploy[0].id
+        gitlab_environment = var.gitlab_environment
       },
       { # CONDITIONAL
         key    = "AWS_SECRET_ACCESS_KEY${var.aws_env_vars_suffix}"
         value  = aws_iam_access_key.deploy[0].secret
         masked = true
+        gitlab_environment = var.gitlab_environment
       },
     ] : [],
     var.extra_gitlab_cicd_variables
